4eec49b317c94e4bbed6a84387b5a4eca7de75aa
praniitpandey <140690806+praniitpandey@users.noreply.github.com>
2026-03-31 00:47:12 +0530

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        if (n == 0) {
+            return 0;
+        }
+        int dp1 = nums[0];
+        if (n == 1) {
+            return dp1;
+        }
+
+        int dp2 = Math.max(nums[0], nums[1]);
+        if (n == 2) {
+            return dp2;
+        }
+
+        int dp3 = Math.max(nums[0] + nums[2], nums[1]);
+        if (n == 3) {
+            return dp3;
+        }
+
+        int dp4 = 0;
+        for (int i = 3; i < n; i++) {
+            dp4 = Math.max(nums[i] + dp2, dp3);
+            dp1 = dp2;
+            dp2 = dp3;
+            dp3 = dp4;
+        }
+
+        return dp4;
+    }
+}
