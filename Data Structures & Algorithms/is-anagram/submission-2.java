c056ab05df1363ccfb73a7d828a705fbbe95af2d
pranit <140690806+praniitpandey@users.noreply.github.com>
2026-03-31 16:28:11 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] sFreq = new int[26];
+        int[] tFreq = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            ++sFreq[s.charAt(i) - 'a'];
+            ++tFreq[t.charAt(i) - 'a'];
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (sFreq[i] != tFreq[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
