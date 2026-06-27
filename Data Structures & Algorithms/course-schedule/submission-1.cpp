66cc6aa6e14372e430a40a98584e22d416a88775
pranit <140690806+praniitpandey@users.noreply.github.com>
2026-06-28 01:31:02 +0530

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.cpp
@@ -0,0 +1,45 @@
+class Solution {
+    vector<vector<int>> adj;
+    vector<int> state;
+    // state 0 : unexplored
+    // state 1 : node in current path
+    // state 2 : node totally explored
+public:
+    bool canFinish(int n, vector<vector<int>>& edges) {
+        adj = vector<vector<int>>(n, vector<int>());
+        state = vector<int>(n, 0);
+
+        for (auto edge : edges) {
+            adj[edge[0]].push_back(edge[1]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (hasCycle(i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+private:
+    bool hasCycle(int u) {
+        if (state[u] == 2) {
+            return false;
+        }
+        if(state[u] == 1) {
+            return true;
+        }
+
+        state[u] = 1;
+
+        for (auto v : adj[u]) {
+            if (hasCycle(v)) {
+                return true;
+            }
+        }
+
+        state[u] = 2;
+        return false;
+    }
+};
