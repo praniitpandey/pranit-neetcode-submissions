ad7104ec4251b51ab72f9b96897a20555dc79f1e
pranit <140690806+praniitpandey@users.noreply.github.com>
2026-07-08 12:35:49 +0530

    Add: parallel-courses - submission-1

---
+++ Data Structures & Algorithms/parallel-courses/submission-1.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int minimumSemesters(int n, vector<vector<int>>& relations) {
+        vector<vector<int>> adj(n + 1, vector<int>());
+        vector<int> indegree(n + 1, 0);
+
+        for (auto& edge : relations) {
+            adj[edge[0]].push_back(edge[1]);
+            ++indegree[edge[1]];
+        }
+
+        queue<int> bfs;
+        for (int u = 1; u <= n; u++) {
+            if (indegree[u] == 0) {
+                bfs.push(u);
+            }
+        }
+
+        int count = 0, layers = 0;
+
+        while (!bfs.empty() && count < n) {
+            int size = bfs.size();
+            ++layers;
+
+            for (int i = 0; i < size; i++) {
+                int u = bfs.front();
+                bfs.pop();
+                ++count;
+
+                for (int v : adj[u]) {
+                    --indegree[v];
+
+                    if (indegree[v] == 0) {
+                        bfs.push(v);
+                    }
+                }
+            }
+        }
+
+        if (count == n) {
+            return layers;
+        }
+        else {
+            return -1;
+        }
+    }
+};
